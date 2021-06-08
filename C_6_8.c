beb94b90b633c18d9296b441b540948c0fa85acd
CQCQCQ12138 <13762544373m@sina.cn>
2021-06-08 18:12:23 +0800

    git config --global http.sslVerify "false"

--- C_6_8.c
+++ C_6_8.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-struct book
-{
-	char book[20];
-	short precie;
-};
-	int main() {
-		struct book b1 = {"C语言程序设计",35};
-		printf("名称是%s\n", b1.book);
-		return 0;
-	}
-
+//#include<stdio.h>
+//struct book
+//{
+//	char book[20];
+//	short precie;
+//};
+//	int main() {
+//		struct book b1 = {"C语言程序设计",35};
+//		printf("名称是%s\n", b1.book);
+//		return 0;
+//	}
+//
