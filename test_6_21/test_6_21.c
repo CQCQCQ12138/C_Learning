adf70fe561aed4de11208899ca74b90681102b6a
CQCQCQ12138 <13762544373m@sina.cn>
2021-06-22 13:57:05 +0800

    。

---
+++ test_6_21/test_6_21.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+//int main() {//创建一个数组，打印它里面所有的元素
+//	int a[] = { 'a','b','c','d'};
+//	for (int j = 0; j < 4; j++)
+//	{
+//		printf("%c",a[j]);
+//	}
+//	return 0;
+//}
+
+//int main() {                        //打印直角三角形
+//	for (int i = 1; i < 6; i++)
+//	{
+//		for (int j = 1; j <=i; j++) {
+//			printf("#");
+//		}
+//		printf("\n");
+//	}
+//	return 0；
+//}
+
+//int main() {  //打印九九乘法表
+//	for (int i = 1; i < 10; i++)
+//	{
+//		for (int j = 1; j < 10; j++)
+//		{
+//			int a = i * j;
+//			printf("%d*%d=*d", i, j, a);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+//int min(int x, int y) {
+//	if (x < y) {
+//		return x;
+//	}
+//	
+//int main() {
+//	int a,b;
+//	printf("请输入两个值");
+//	scanf_s("%d %d",&a,&b);
+//	int c=min(a,b);
+//	printf("%d",c);
+//	return 0;
+//}
+
+//int main() {
+//	int a, b, c;
+//	printf("请输入一个字母，两个数字");
+//	scanf_s("%c ",&a);
+//	scanf_s("%d %d",&b,&c);
+//	for (int i = 0; i <=b; i++)
+//	{
+//		for (int j = 0; j<=c-1; j++)
+//		{
+//			printf("%c",a);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
